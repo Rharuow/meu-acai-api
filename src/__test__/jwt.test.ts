583c1cf4f9c76debb8c7b159e7b43275df3085b7
Harysson <haryssonsoares@gmail.com>
2023-11-16 08:00:32 -0300

    feat: add jwt test

--- src/__test__/jwt.test.ts
+++ src/__test__/jwt.test.ts
@@ -1,6 +1,37 @@
+import request from "supertest";
 import { encodeSha256 } from "@/lib/crypto";
 import { prismaClient } from "@/lib/prisma";
+import { app } from "@/app";
 
 describe("Signin route", () => {
-  test("when access POST route '/api/v1/signgin' contains in body the username and password correclty", () => {});
+  test("when access POST route '/api/v1/signgin' contains in body the username and password correclty", async () => {
+    const requestBody = {
+      username: "Test Admin",
+      password: "123",
+    };
+    try {
+      const adminRole = await prismaClient.role.findFirstOrThrow({
+        where: { name: "ADMIN" },
+      });
+      const user = await prismaClient.user.create({
+        data: {
+          name: "Test Admin",
+          password: encodeSha256("123"),
+          roleId: adminRole.id,
+        },
+      });
+
+      const response = await request(app)
+        .post("/api/v1/sign")
+        .send(requestBody)
+        .set("Accept", "application/json")
+        .expect("Content-Type", /json/)
+        .expect(200);
+
+      expect(response.body).toHaveProperty("token");
+    } catch (error) {
+      console.log(error);
+      throw new Error(error.message);
+    }
+  });
 });
