08dd84075275a2f4f717e11e3d4558afe270e75c
Harysson <haryssonsoares@gmail.com>
2023-11-16 07:26:15 -0300

    wip: describing test to jwt

---
+++ src/__test__/jwt.test.ts
@@ -0,0 +1,6 @@
+import { encodeSha256 } from "@/lib/crypto";
+import { prismaClient } from "@/lib/prisma";
+
+describe("Signin route", () => {
+  test("when access POST route '/api/v1/signgin' contains in body the username and password correclty", () => {});
+});
