d55edb4ef332543c348fb34d262f0dbc71389113
Harysson <haryssonsoares@gmail.com>
2023-12-17 16:41:43 -0300

    improvement: add test to create topping

---
+++ prisma/migrations/20231217182752_add_default_value_to_unit_field/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "Topping" ALTER COLUMN "unit" SET DEFAULT 'Unidades';
---
+++ src/__test__/routes/toppings/crud.test.ts
@@ -0,0 +1,130 @@
+import { createAdminRoleIfNotExist } from "@/__test__/presets/createAdminRoleIfNotExists";
+import { createClientRoleIfNotExist } from "@/__test__/presets/createClientRoleIfNotExists";
+import { createMemberRoleIfNotExist } from "@/__test__/presets/createMemberRoleIfNotExists";
+import { app } from "@/app";
+import { prismaClient } from "@libs/prisma";
+import { Admin, Client, Member, Role, User } from "@prisma/client";
+import { createAdmin } from "@repositories/user/admin";
+import { createClient } from "@repositories/user/client";
+import { createMember } from "@repositories/user/member";
+import request from "supertest";
+
+let adminAuthenticated: User & { role: Role; admin: Admin };
+let clientAuthenticated: User & { role: Role; client: Client };
+let memberAuthenticated: User & { role: Role; member: Member };
+
+let accessTokenAsAdmin: string;
+let refreshTokenAsAdmin: string;
+
+let accessTokenAsClient: string;
+let refreshTokenAsClient: string;
+
+let accessTokenAsMember: string;
+let refreshTokenAsMember: string;
+
+beforeAll(async () => {
+  const [roleIdAdmin, roleIdClient, roleIdMember] = await Promise.all([
+    createAdminRoleIfNotExist(),
+    createClientRoleIfNotExist(),
+    createMemberRoleIfNotExist(),
+  ]);
+
+  const [adminCreated, clientCreated] = await Promise.all([
+    createAdmin({
+      name: "Test Admin to test Toppings",
+      password: "123",
+      roleId: roleIdAdmin,
+    }),
+    createClient({
+      name: "Test Client to test Toppings",
+      password: "123",
+      roleId: roleIdClient,
+      address: {
+        house: "Test House to test toppings",
+        square: "Test Square to test toppings",
+      },
+    }),
+  ]);
+
+  adminAuthenticated = adminCreated;
+  clientAuthenticated = clientCreated;
+  memberAuthenticated = await createMember({
+    name: "Test Member to test toppings",
+    password: "123",
+    roleId: roleIdMember,
+    clientId: clientAuthenticated.client.id,
+  });
+
+  const [
+    responseSignInAsAdmin,
+    responseSignInAsClient,
+    responseSignInAsMember,
+  ] = await Promise.all([
+    request(app)
+      .post("/api/v1/signin")
+      .send({ name: adminAuthenticated.name, password: "123" })
+      .set("Accept", "application/json")
+      .expect(200),
+    request(app)
+      .post("/api/v1/signin")
+      .send({ name: clientAuthenticated.name, password: "123" })
+      .set("Accept", "application/json")
+      .expect(200),
+    request(app)
+      .post("/api/v1/signin")
+      .send({ name: memberAuthenticated.name, password: "123" })
+      .set("Accept", "application/json")
+      .expect(200),
+  ]);
+
+  accessTokenAsAdmin = "Bearer " + responseSignInAsAdmin.body.accessToken;
+  refreshTokenAsAdmin = "Bearer " + responseSignInAsAdmin.body.refreshToken;
+
+  accessTokenAsClient = "Bearer " + responseSignInAsClient.body.accessToken;
+  refreshTokenAsClient = "Bearer " + responseSignInAsClient.body.refreshToken;
+
+  accessTokenAsMember = "Bearer " + responseSignInAsMember.body.accessToken;
+  refreshTokenAsMember = "Bearer " + responseSignInAsMember.body.refreshToken;
+});
+
+afterAll(async () => {
+  await prismaClient.user.deleteMany({
+    where: {
+      id: {
+        in: [
+          adminAuthenticated.id,
+          clientAuthenticated.id,
+          memberAuthenticated.id,
+        ],
+      },
+    },
+  });
+});
+
+describe("CRUD TOPPING RESOURCE", () => {
+  const baseUrl = "/api/v1/resources/toppings";
+  const setIdInBaseUrl = (id: string) => `${baseUrl}/${id}`;
+  describe("CREATE TOPPING AS ADMIN", () => {
+    const toppingCreate = {
+      name: "Test Topping created as Admin",
+      price: 12.99,
+      amount: 2,
+    };
+
+    test(
+      `When an Admin access POST ${baseUrl}` +
+        " sending name, price and amount in the request body" +
+        " then the respnse status code is 200 and the adminId must be equal to the admin authenticated",
+      async () => {
+        const response = await request(app)
+          .post(baseUrl)
+          .set("authorization", accessTokenAsAdmin)
+          .set("refreshToken", refreshTokenAsAdmin)
+          .send(toppingCreate)
+          .expect(200);
+
+        return expect(response.statusCode).toEqual(200);
+      }
+    );
+  });
+});
